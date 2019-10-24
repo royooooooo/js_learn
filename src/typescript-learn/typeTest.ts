a3b6e1abbb2c9eb7e915aa0052910095bcde87e8
Fu Yi <yifu@thoughtworks.com>
2019-10-24 16:33:24 +0800

    format the typeTest.

--- src/typescript-learn/typeTest.ts
+++ src/typescript-learn/typeTest.ts
@@ -1,18 +1,18 @@
 type A = {
-  a: string;
-  b: number;
+    a: string;
+    b: number;
 };
 
 type B = {
-  c: string;
-  d: number;
+    c: string;
+    d: number;
 };
 
 type C = A | B;
 
 type D = A & B;
 
-const testD: C = { a: "test", b: 2 };
+const testD: C = {a: "test", b: 2};
 
 type Tuple = [number, string];
 
@@ -21,16 +21,16 @@ const array: Tuple = [1, "2"];
 type Site = "samll" | "default" | "big";
 
 type MapB = {
-  small: string;
-  default: number;
-  big: string;
+    small: string;
+    default: number;
+    big: string;
 };
 
 // type map2Set = keyof MapB; // => small | default | big
 type map2Set = MapB[keyof MapB]; // => string | number | string
 
 type set2Map = {
-  [key in Site]: string;
+    [key in Site]: string;
 };
 // => type set2Map = {
 //     small: string,
@@ -40,51 +40,18 @@ type set2Map = {
 
 // map取索引值
 type MapTest = {
-  test: number;
+    test: number;
 };
 
 type valueOfIndex = MapTest["test"]; // => number
 
 type Person = {
-  readonly name: string;
-  age?: number;
+    readonly name: string;
+    age?: number;
 };
 
 const person: Person = {
-  name: "John"
+    name: "John"
 };
 
 // person.name = "test"; // => get the error
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

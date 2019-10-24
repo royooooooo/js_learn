ba6474cca24da03b9989c60b0c984665740623a7
Fu Yi <yifu@thoughtworks.com>
2019-10-24 16:32:16 +0800

    add some ts sample.

---
+++ src/tdd-dojo/__tests__/FizzBuzz.test.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: In User Settings 
+ * @Author: your name
+ * @Date: 2019-08-08 22:11:46
+ * @LastEditTime: 2019-08-08 22:13:10
+ * @LastEditors: Please set LastEditors
+ */
+describe('FizzBuzz', () => {
+    it('should return 1 given 1', () => {
+        expect("1").toEqual("1");
+    })
+    
+})
---
+++ src/typescript-learn/typeTest.ts
@@ -0,0 +1,90 @@
+type A = {
+  a: string;
+  b: number;
+};
+
+type B = {
+  c: string;
+  d: number;
+};
+
+type C = A | B;
+
+type D = A & B;
+
+const testD: C = { a: "test", b: 2 };
+
+type Tuple = [number, string];
+
+const array: Tuple = [1, "2"];
+
+type Site = "samll" | "default" | "big";
+
+type MapB = {
+  small: string;
+  default: number;
+  big: string;
+};
+
+// type map2Set = keyof MapB; // => small | default | big
+type map2Set = MapB[keyof MapB]; // => string | number | string
+
+type set2Map = {
+  [key in Site]: string;
+};
+// => type set2Map = {
+//     small: string,
+//     default: string,
+//     big: string
+// }
+
+// map取索引值
+type MapTest = {
+  test: number;
+};
+
+type valueOfIndex = MapTest["test"]; // => number
+
+type Person = {
+  readonly name: string;
+  age?: number;
+};
+
+const person: Person = {
+  name: "John"
+};
+
+// person.name = "test"; // => get the error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
